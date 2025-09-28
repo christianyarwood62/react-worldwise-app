5cc647eabc6afadd6f7fa3c01510efe807e90b36
Christian Yarwood <christianyarwood@gmail.com>
2025-09-28 20:27:28 +0100

    Add PageNav component to every page except login

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,20 @@
 import { NavLink } from "react-router-dom"; // NavLink adds an active class to the nav link when selected, which a normal Link doesnt do
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/Pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
