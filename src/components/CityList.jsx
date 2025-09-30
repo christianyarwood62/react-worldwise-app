dd4daaa46cdfba7cf4170d5fa348c94a60e38f88
Christian Yarwood <christianyarwood@gmail.com>
2025-09-30 08:38:30 +0100

    Add new Message component to city list if empty

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,14 @@
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add your first city by clicking city on a map" />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
