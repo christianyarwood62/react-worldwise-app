f31ba88f624548dfdf09810e40a5608ce37d2679
Christian Yarwood <christianyarwood@gmail.com>
2025-09-30 08:31:42 +0100

    add 3 cities to cityList

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,14 @@
-function CityItem() {
-  return <li>city</li>;
+import styles from "./CityItem.module.css";
+
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+    </li>
+  );
 }
 
 export default CityItem;
