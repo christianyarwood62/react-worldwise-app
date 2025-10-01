ac68be6a401370c37b3e2c1899ff7dcbddc54148
Christian Yarwood <christianyarwood@gmail.com>
2025-10-01 09:09:58 +0100

    Add types to Form buttons

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,15 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
+  const lat = searchParams.get("lat"); // searchParams and get looks into the URL, the get string has to match what is in the URL.
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
