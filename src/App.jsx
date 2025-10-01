9532be7cec87550d7134d3123b223d76542bef53
Christian Yarwood <christianyarwood@gmail.com>
2025-10-01 09:24:13 +0100

    Add Navigate component to redirect to Cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import Product from "./pages/Product";
@@ -42,10 +42,9 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          {/* Navigate is a native component that allows you to redirect to one of the routes */}
+          {/* fixes issue with clicking back button, by replacing current element in the history stack */}
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
