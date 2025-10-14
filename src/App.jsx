b1d59f7d3f4084e56c9ec7bb9c0f84de88185099
Christian Yarwood <christianyarwood@gmail.com>
2025-10-14 10:27:27 +0100

    Add login and logout component

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,33 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
     <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            {/* Navigate is a native component that allows you to redirect to one of the routes */}
-            {/* fixes issue with clicking back button, by replacing current element in the history stack */}
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route path="cities" element={<CityList />} />
-            {/* the id in the path can be named anything, this determines what the key in the object is in City.jsx where useParams() is used */}
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              {/* Navigate is a native component that allows you to redirect to one of the routes */}
+              {/* fixes issue with clicking back button, by replacing current element in the history stack */}
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              {/* The : is a dynamic parameter and makes it accessible by useParams */}
+              {/* the id in the path can be named anything, this determines what the key in the object is in City.jsx where useParams() is used */}
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </CitiesProvider>
   );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,18 @@
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
+import User from "../components/User";
+import { useAuth } from "../contexts/FakeAuthContext";
 
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
+  const { user } = useAuth();
+
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
